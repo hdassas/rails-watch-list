3b730032531d695b41d88563448224c6e19407b3
Hugo Dassas <hugo.dassas@gmail.com>
2026-08-14 12:23:55 +0200

    List avec photos upload sur cloudinary

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ source "https://rubygems.org"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.1.3", ">= 8.1.3.1"
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
-#gem "propshaft"
+# gem "propshaft"
+
+gem "cloudinary"
 
 gem "sprockets-rails"
 gem "sassc-rails"
@@ -66,5 +68,5 @@ group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
 end
-gem 'rspec-rails', group: [ :test ]
-gem 'rails-controller-testing', group: [ :test ]
+gem "rspec-rails", group: [ :test ]
+gem "rails-controller-testing", group: [ :test ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+require "open-uri"
+require "json"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+base_url = "https://image.tmdb.org/t/p/w500"
+response = URI.open(url).read
+data = JSON.parse(response)
+movies = data["results"]
+movies.each do |movie|
+  new_movie = Movie.new(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "#{base_url}#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+  new_movie.save
+end
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
